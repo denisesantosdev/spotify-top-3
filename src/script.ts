6c5c7ca36f5bc3bde22bfae7b3a2c51593702f0f
Denise Santos <denisesantosdev@gmail.com>
2024-06-07 16:23:17 -0300

    attempt to fix deploy problem

--- src/script.ts
+++ src/script.ts
@@ -4,7 +4,7 @@ const clientId = "16ed7b7faf094b429335cfc77338edd1";
 const scopes = "user-read-private user-read-email user-top-read"
 const params = new URLSearchParams(window.location.search);
 //const redirect_uri = "http://localhost:5173/callback";
-const redirect_uri = "https://spotify-top-3.netlify.app";
+const redirect_uri = "https://spotify-top-3.netlify.app/callback";
 const code = params.get("code");
 
 document.getElementById("login-btn")?.addEventListener("click", loginToSpotify);
@@ -18,7 +18,6 @@ async function loginToSpotify() {
 async function loadAccessToken() {
   let accessToken 
   if (code) {
-    console.log(code);
 
     if(!localStorage.getItem("access_token")){
       accessToken = (await getAccessToken(clientId, code))
