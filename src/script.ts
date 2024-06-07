51985895d8149b18fb8d09b869f6b92ca760fda6
Denise Santos <denisesantosdev@gmail.com>
2024-06-06 22:23:50 -0300

    attempt to fix deploy problem

--- src/script.ts
+++ src/script.ts
@@ -130,6 +130,8 @@ async function redirectToAuthCodeFlow(clientId: string) {
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
 
+  console.log(params);
+
   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
